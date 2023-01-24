42589a97ee3c3af07f294d5af006e90d668d2bad
MarcMeRu11 <72763674+MarcMeRu11@users.noreply.github.com>
2023-01-24 19:13:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,48 @@ def Download_high_resolution(link):
     except:
         print("An error ocurred.")
 
-link = input("Put the YouTube link here. URL:")
-Download_high_resolution(link)
+def Download_audio_only(link):
+    objeto = YouTube(link)
+    objeto = objeto.streams.get_audio_only()
+    try:
+        objeto.download()
+        print("The video was downloaded.")
+    except:
+        print("An error ocurred.")
+
+def Download_low_resolution(link):
+    objeto = YouTube(link)
+    objeto = objeto.streams.get_lowest_resolution()
+    try:
+        objeto.download()
+        print("The video was succesfully downloaded.")
+    except:
+        print("Somethig went wrong.")
+while True: 
+    link = input("Put the YouTube link here. URL:")
+    try:
+        obj = YouTube(link)
+        titulo = obj.title
+        mode = input("Select a mode:\n-High resolution /.mp4 (H).\n-Low resolution /.mp4 (L).\n-Audio only /.mp3(A).")
+        if mode == "H":
+            modo = "high Resolution"
+        elif mode == "L":
+            modo = "low resoltion"
+        elif mode == "A":
+            modo = "audio only"
+        print('The video is titled: "'+ titulo + '".\nMode:"' + modo + '".')
+        q1 = input( "Do you want to download it? (Y/N).")
+        if q1 == "Y":
+            break
+        elif q1 == "N":
+            "Canceled, try again."
+        else:
+            print("Something went wrong.")
+    except:
+        print("Something went wrong.")
+if mode == "H":
+    Download_high_resolution(link)
+elif mode == "L":
+    Download_low_resolution(link)
+elif mode == "A":
+    Download_audio_only(link)

ef7c60422c6cc575224654bdd1b7a5de2ae3b7f4
MarcMeRu11 <72763674+MarcMeRu11@users.noreply.github.com>
2023-01-24 20:58:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 from pytube import YouTube
 
-def Download_high_resolution(link):
+def Download(link,mode):
     objeto = YouTube(link)
-    objeto = objeto.streams.get_highest_resolution()
+    if mode == "H":
+        objeto = objeto.streams.get_highest_resolution()
+    elif mode == "L": 
+        objeto = objeto.streams.get_lowest_resolution()
+    elif mode == "A":
+        objeto = objeto.streams.get_audio_only()
     try:
         objeto.download()
         print("The video was downloaded.")
     except:
         print("An error ocurred.")
 
-link = input("Put the YouTube link here. URL:")
-Download_high_resolution(link)
+while True: 
+    link = input("Put the YouTube link here. URL:")
+    try:
+        obj = YouTube(link)
+        titulo = obj.title
+        mode = input("Select a mode:\n-High resolution /.mp4 (H).\n-Low resolution /.mp4 (L).\n-Audio only /.mp3(A).\n>>>")
+        if mode == "H":
+            modo = "high Resolution"
+        elif mode == "L":
+            modo = "low resoltion"
+        elif mode == "A":
+            modo = "audio only"
+        segundos = obj.length
+        minutos = int(segundos / 60)
+        segundos -= minutos * 60
+        print('The video is titled: "'+ titulo + '".\nMode:"' + modo + '".\nThe length is:', str(minutos)+":"+str(segundos),"minutes.")
+        q1 = input( "Do you want to download it? (Y/N).\n>>>")
+        if q1 == "Y":
+            break
+        elif q1 == "N":
+            "Canceled, try again."
+        else:
+            print("Something went wrong.")
+    except:
+        print("Something went wrong.")
+Download(link,mode)
